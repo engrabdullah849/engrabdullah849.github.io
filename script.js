024fe813e6ad382c23ad2aa32deba2d766c0b443
engrabdullah849 <mabdullahsattar.me@gmail.com>
2026-09-25 01:04:32 -0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  if (location.hash) {
-    const target = document.querySelector(location.hash);
-    if (target) setTimeout(() => target.scrollIntoView(), 50);
-  }
-});
